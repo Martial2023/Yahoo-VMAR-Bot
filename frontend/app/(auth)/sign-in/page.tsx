9ee0ba0f6e3fe2d4d2c209f1d06758c2951aa295
Martial Hilarion Avadra <martialhilarionavadra@gmail.com>
2026-04-25 22:35:48 +0200

    fix type error 6

--- frontend/app/(auth)/sign-in/page.tsx
+++ frontend/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react"
 import LoginForm from "../_components/LoginForm"
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
+        <Suspense>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   )
